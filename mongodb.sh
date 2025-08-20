18544eeea0a01ba481af3a755f9db2e5893f120c
kirupa003 <kiruparahas@gmail.com>
2025-08-20 16:54:50 +0530

    Delete mongodb.sh

--- mongodb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-curl -fsSL https://pgp.mongodb.com/server-7.0.asc | sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-sudo apt update
-sudo apt install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
-sudo systemctl status mongod
-echo "run mongosh"
